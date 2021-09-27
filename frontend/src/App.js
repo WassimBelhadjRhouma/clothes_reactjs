288e195af7176a2aebbeb803f0b824fa53b6bff3
Wassim Belhadj Rhouma <wassimbelhadjrhouma@gmail.com>
2021-09-27 03:10:10 +0200

    second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container} from "react-bootstrap"
 
 function App() {
   return (
     <>
+    <Header/>
+    <main>
+    <Container>
      <h1>wasssi mbhe </h1>
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Container} from "react-bootstrap"
+const Header = () => {
+    return (
+        <header>
+            <Container>
+            header is working
+            </Container>
+        </header>
+    )
+}
+
+export default Header
